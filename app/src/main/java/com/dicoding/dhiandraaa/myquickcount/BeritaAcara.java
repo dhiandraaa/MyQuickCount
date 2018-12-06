e6aa1661c1f23d8af0bd2efc00905fb9d2041d4a
dhiandraaa <d.adithya.a@gmail.com>
2018-12-06 18:36:37 +0700

    Camera Berita Acara

--- app/src/main/java/com/dicoding/dhiandraaa/myquickcount/BeritaAcara.java
+++ app/src/main/java/com/dicoding/dhiandraaa/myquickcount/BeritaAcara.java
@@ -1,9 +1,21 @@
 package com.dicoding.dhiandraaa.myquickcount;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class BeritaAcara extends AppCompatActivity {
+
+    private ImageButton btCamera;
+    private ImageView ivCamera;
+    private static final String TAG = BeritaAcara.class.getSimpleName();
+    private static final int CAMERA_REQUEST_CODE = 7777;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +23,33 @@ public class BeritaAcara extends AppCompatActivity {
         setContentView(R.layout.activity_berita_acara);
 
         setTitle("Berita Acara");
+
+        btCamera = (ImageButton) findViewById(R.id.bt_camera);
+        ivCamera = (ImageView) findViewById(R.id.iv_camera);
+
+        btCamera.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //intent khusus untuk menangkap foto lewat kamera
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, CAMERA_REQUEST_CODE);
+            }
+        });
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case(CAMERA_REQUEST_CODE) :
+                if(resultCode == Activity.RESULT_OK)
+                {
+                    // result code sama, save gambar ke bitmap
+                    Bitmap bitmap;
+                    bitmap = (Bitmap) data.getExtras().get("data");
+                    ivCamera.setImageBitmap(bitmap);
+                }
+                break;
+        }
     }
 }
